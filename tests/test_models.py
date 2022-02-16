462f46d25f6ec26c4b62576c67785f4bf18a7f73
h4pZ <h4pz@protonmail.com>
2022-02-16 15:14:17 +0100

    Update test_patient_normalise to test for input checking

--- tests/test_models.py
+++ tests/test_models.py
@@ -102,12 +102,29 @@ def test_daily_min_string():
             [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
             None,
         ),
+        (
+            'hello',
+            None,
+            TypeError,
+        ),
+        (
+            3,
+            None,
+            TypeError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        )
     ])
 def test_patient_normalise(test, expected, raises):
     """Test normalisation works for arrays of one and positive integers."""
     from inflammation.models import patient_normalise
+    if isinstance(test, list):
+        test = np.array(test)
     if raises:
         with pytest.raises(raises):
-            npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+            npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
     else:
-        npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+        npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
